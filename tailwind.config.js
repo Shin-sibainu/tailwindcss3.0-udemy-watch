a28bbab0aed8d597c33ef5d8e2ba3c9f2d84217e
Shin-sibainu <shincode0712@gmail.com>
2022-05-29 11:13:39 +0900

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "udemy-purple": "#5267DF",
+        "udemy-red": "#FA5959",
+        "udemy-blue": "#242A45",
+        "udemy-grey": "#9194A2",
+        "udemy-white": "#f7f7f7",
+      },
+    },
+    fontFamily: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+};
